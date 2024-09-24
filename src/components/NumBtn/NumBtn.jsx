202f5fbf719c7ad082eb1317485b9b18212a37ba
wtei7 <m45a1@dgsw.hs.kr>
2024-09-24 23:01:24 +0900

    "foundation"

---
+++ src/components/NumBtn/NumBtn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NumBtn=()=>{
+    return(
+        <div></div>
+    )
+}
+
+export default NumBtn;
---
+++ src/components/Wash/Wash.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Wash=()=>{
+    return(
+        <div></div>
+    )
+}
+
+export default Wash;
