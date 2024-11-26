d8645884551ed21e914a2c6118f5719225301474
wtei7 <m45a1@dgsw.hs.kr>
2024-11-27 08:17:45 +0900

    add :: 폰트, 사진, toastify 추가

--- public/index.js
+++ public/index.js
@@ -6,4 +6,8 @@ const GlobalStyle = createGlobalStyle`
         font-family: Pretendard-Medium;
         src: url('../src/assets/Fonts/Pretendard-Medium.otf');
     }
+    @font-face {
+        font-family: Pretendard-Black;
+        src: url('../src/assets/Fonts/Pretendard-Black.otf');
+    }
 `
