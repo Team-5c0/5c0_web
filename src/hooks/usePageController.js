427aca87400054b26761a1d0e22ef2e161c6cd16
wtei7 <m45a1@dgsw.hs.kr>
2024-12-26 18:01:57 +0900

    fix :: 이중 훅 문제 수정

--- src/hooks/usePageController.js
+++ src/hooks/usePageController.js
@@ -1,18 +1,38 @@
 import {useNavigate} from "react-router-dom";
-import useWashing from "./useWashing";
+import {useState} from "react";
+import axios from "axios";
 
 const usePageController = (page = '', active, error) => {
+    if (page === '/end') {
 
+    }
     const navigate = useNavigate();
-    const washing = useWashing();
     const goBack = () => {navigate(-1)}
     const goNext = () => {navigate(page)}
     const goNextHandler = () => {
         if (!active) alert(error)
         else if (page === '/end') {
-            washing.postData()
-            if (washing.done) goNext()
-            else alert('학번이 존재하지 않습니다')
+            const getValue = (name) => {
+                return Number(localStorage.getItem(name));
+            }
+            const washerId = getValue("washerId");
+            const userId = getValue("userId");
+            const washMinutes = getValue("washMinutes");
+            const postData = () => {
+                axios.post(`${process.env.REACT_APP_SERVER_URL}/wash/usewasher`, null, {
+                    params: {
+                        washerId: washerId,
+                        userId: userId,
+                        washMinutes: washMinutes,
+                    }
+                }).then(() => {
+                    goNext()
+                }).catch(err => {
+                    console.log(err.status);
+                    alert('학번이 존재하지 않습니다')
+                });
+            }
+            postData()
         }
         else {goNext()}
     }
--- src/hooks/useWashing.js
+++ src/hooks/useWashing.js
@@ -1,31 +1,31 @@
-import axios from "axios";
-import {useState} from "react";
-
-const useWashing = () => {
-    const [done, setDone] = useState(false);
-    const getValue = (name) => {
-        return Number(localStorage.getItem(name));
-    }
-    const washerId = getValue("washerId");
-    const userId = getValue("userId");
-    const washMinutes = getValue("washMinutes");
-
-    const postData = () => {
-        setDone(true);
-        axios.post(`${process.env.REACT_APP_SERVER_URL}/wash/usewasher`, null, {
-            params: {
-                washerId: washerId,
-                userId: userId,
-                washMinutes: washMinutes,
-            }
-        }).then(() => {
-            setDone(true);
-        }).catch(err => {
-            console.log(err.status);
-            setDone(false);
-        });
-    }
-
-    return {done,postData};
-}
-export default useWashing;
+// import axios from "axios";
+// import {useState} from "react";
+//
+// const useWashing = () => {
+//     const [done, setDone] = useState(false);
+//     const getValue = (name) => {
+//         return Number(localStorage.getItem(name));
+//     }
+//     const washerId = getValue("washerId");
+//     const userId = getValue("userId");
+//     const washMinutes = getValue("washMinutes");
+//
+//     const postData = () => {
+//         setDone(true);
+//         axios.post(`${process.env.REACT_APP_SERVER_URL}/wash/usewasher`, null, {
+//             params: {
+//                 washerId: washerId,
+//                 userId: userId,
+//                 washMinutes: washMinutes,
+//             }
+//         }).then(() => {
+//             setDone(true);
+//         }).catch(err => {
+//             console.log(err.status);
+//             setDone(false);
+//         });
+//     }
+//
+//     return {done,postData};
+// }
+// export default useWashing;
